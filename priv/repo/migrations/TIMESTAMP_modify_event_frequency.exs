9d671962c76051aa4541693be5c9ab1049831442
holden <holden@razrfly.com>
2025-02-13 15:13:30 +0100

    dumb

--- priv/repo/migrations/TIMESTAMP_modify_event_frequency.exs
+++
@@ -1,9 +0,0 @@
-defmodule TriviaAdvisor.Repo.Migrations.ModifyEventFrequency do
-  use Ecto.Migration
-
-  def change do
-    alter table(:events) do
-      modify :frequency, :string
-    end
-  end
-end
