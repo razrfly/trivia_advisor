8ef7fcd8909d1055e3e737f4ba35df7bfc833893
holden <holden@razrfly.com>
2025-02-13 12:57:11 +0100

    confused

--- priv/repo/migrations/20250213000000_add_metadata_to_venues.exs
+++ priv/repo/migrations/20250213000000_add_metadata_to_venues.exs
@@ -2,10 +2,6 @@ defmodule TriviaAdvisor.Repo.Migrations.AddMetadataToVenues do
   use Ecto.Migration
 
   def change do
-    alter table(:venues) do
-      add :metadata, :jsonb
-    end
-
     # Add GIN index for potential future JSONB queries
     execute(
       "CREATE INDEX venues_metadata_idx ON venues USING GIN (metadata jsonb_path_ops)",
