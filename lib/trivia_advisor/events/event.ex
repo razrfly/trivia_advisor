c51cf9b185bf383778fbf92ab8602fde408bc4b5
holden <holden@razrfly.com>
2025-02-13 13:53:09 +0100

    migrations

--- lib/trivia_advisor/events/event.ex
+++ lib/trivia_advisor/events/event.ex
@@ -2,11 +2,13 @@ defmodule TriviaAdvisor.Events.Event do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @frequencies ~w(weekly biweekly monthly irregular)a
+
   schema "events" do
     field :name, :string
     field :day_of_week, :integer
     field :start_time, :time
-    field :frequency, :integer
+    field :frequency, Ecto.Enum, values: @frequencies, default: :weekly
     field :entry_fee_cents, :integer
     field :description, :string
 
@@ -22,4 +24,18 @@ defmodule TriviaAdvisor.Events.Event do
     |> cast(attrs, [:name, :day_of_week, :start_time, :frequency, :entry_fee_cents, :description, :venue_id])
     |> validate_required([:day_of_week, :start_time, :frequency, :venue_id])
   end
+
+  @doc """
+  Parses frequency text into the correct enum value.
+  """
+  def parse_frequency(text) when is_binary(text) do
+    text = String.downcase(text)
+    cond do
+      Regex.match?(~r/every\s+week|weekly|each week/i, text) -> :weekly
+      Regex.match?(~r/every\s+2\s+weeks?|bi.?weekly|fortnightly/i, text) -> :biweekly
+      Regex.match?(~r/every\s+month|monthly/i, text) -> :monthly
+      true -> :irregular
+    end
+  end
+  def parse_frequency(_), do: :irregular
 end
