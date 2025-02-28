891f386c48eefa4ea2cb0bc83359e0ea4e4d0111
holden <holden@razrfly.com>
2025-02-28 21:40:53 +0100

    images working

--- lib/trivia_advisor/uploaders/google_place_image.ex
+++ lib/trivia_advisor/uploaders/google_place_image.ex
@@ -23,13 +23,25 @@ defmodule TriviaAdvisor.Uploaders.GooglePlaceImage do
     "uploads/google_place_images/#{venue_slug || venue_id}"
   end
 
+  # Handle map format
+  def storage_dir(_version, {_file, %{venue_id: venue_id, venue_slug: venue_slug}}) do
+    "uploads/google_place_images/#{venue_slug || venue_id}"
+  end
+
   # Provide a default URL if there hasn't been a file uploaded
   def default_url(_version, _scope) do
     "https://placehold.co/600x400/png"
   end
 
-  # Generate a unique filename
+  # Generate a unique filename - tuple format
   def filename(version, {file, {_venue_id, _venue_slug, position}}) do
+    file_name = Path.rootname(file.file_name)
+    file_extension = Path.extname(file.file_name)
+    "#{version}_#{file_name}_#{position}#{file_extension}"
+  end
+
+  # Generate a unique filename - map format
+  def filename(version, {file, %{position: position}}) do
     file_name = Path.rootname(file.file_name)
     file_extension = Path.extname(file.file_name)
     "#{version}_#{file_name}_#{position}#{file_extension}"
